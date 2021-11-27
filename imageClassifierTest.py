fc61c541595b02716d266ee6bddd05882cf4709a
patrick <phesse001@csbsju.edu>
2021-11-26 18:44:08 -0600

    added trained model and removed some test code

--- imageClassifierTest.py
+++ imageClassifierTest.py
@@ -1,5 +1,4 @@
-import imageClassifierTrain as ic
-# imports
+from imageClassifierTrain import Net
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -11,8 +10,7 @@ PATH = './cifar_net.pth'
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# To load model
-net = ic.Net()
+net = Net()
 net.load_state_dict(torch.load(PATH))
 
 transform = transforms.Compose(
@@ -24,51 +22,24 @@ testset = torchvision.datasets.CIFAR10(root='./data/data_cifar_test', train=Fals
 
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                              shuffle=False, num_workers=2)
-# show a batch of images with labels (test set)
-dataiter = iter(testloader)
-images, labels = dataiter.next()
 
-# print images
-ic.imgshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-#pass a batch of images through the network
-outputs = net(images)
-
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
-
-# test on whole dataset
+# test on testset
 correct = 0
 total = 0
-# no_grad() to make sure that gradients aren't computed
+
 with torch.no_grad():
+
     for data in testloader:
+
         images, labels = data
+        
         outputs = net(images)
+
         _, predicted = torch.max(outputs.data, 1)
+
         total += labels.size(0)
+        
         correct += (predicted == labels).sum().item()
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
-
-# print the error of a particular class
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(4):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
-
-
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
--- imageClassifierTrain.py
+++ imageClassifierTrain.py
@@ -2,34 +2,22 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
-
+import torch.nn as nn
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load and Normalize the CIFARR10 training and test datasets using torchvision
-# Define a convolutional nn
-# Define a loss function
-# Train nn
-# Test nn
-
-# Note: for pretrained models can call torchvision.models.<model_name>(pretrained=true)
-
-# Define Conv Net
-import torch.nn as nn
-import torch.nn.functional as F
-
-#####################################CLASS AND HELPER FUNCTION###############################
-
+# Defining a simple convoluational neural network
 class Net(nn.Module):
     def __init__(self):
         # super constructor is here to inherit attributes from nn.Module
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 5)
+        self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        self.conv2 = nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5)
+        self.fc1 = nn.Linear(in_features = 16 * 5 * 5, out_features = 120)
+        self.fc2 = nn.Linear(in_features = 120, out_features = 84)
+        self.fc3 = nn.Linear(in_features = 84, out_features = 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -51,6 +39,10 @@ def imgshow(img):
 
 def main():
     print("Number of threads " + str(torch.get_num_threads()))
+
+    # The output of torchvision datasets are PILImage images of range [0, 1]. We transform them to Tensors of normalized range [-1, 1].
+    # This will first transform from a PIL image to a tensor
+    # Then this will normalize each chanel to have a mean of 0.5 and standard deviation of 0.5
     transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -60,13 +52,13 @@ def main():
 
     trainset = torchvision.datasets.CIFAR10(root='./data/data_cifar_train', train=True,
                                             download=False, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=5,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                               shuffle=True, num_workers=4)
     dataiter = iter(trainloader)
     images,labels = dataiter.next()
 
     imgshow(torchvision.utils.make_grid(images))
-    print(' '.join('%5s' % classes[labels[j]] for j in range(5)))
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
     net = Net()
 
@@ -79,19 +71,21 @@ def main():
         # loop over the dataset multiple times (twice)
         running_loss = 0
         for num, data in enumerate(trainloader, 0):
+
             inputs,labels = data
 
             optimizer.zero_grad() #zero the parameter gradients, otherwise they will add up from previous passes
-
-            # forward -> backward -> optimize
-            outputs = net.forward(inputs)
+            
+            outputs = net.forward(inputs) # forward pass of current image
+            
             loss = criterion(outputs, labels)
-            loss.backward() 
-            optimizer.step() #applys param updates
 
-            running_loss += loss.item()
+            loss.backward() # calculate partial derivatives 
 
-            # print every 2000 (1999 % 2000 == 1999)
+            optimizer.step() # update weights of the network
+
+            running_loss += loss.item() # to get a more meaningful idea of how our loss is improving just check the sum every n steps
+
             if num % 2000 == 1999:
                 print('[%d, %5d] loss: %.3f' %(epoch + 1, num + 1, running_loss / 2000))
                 running_loss = 0
